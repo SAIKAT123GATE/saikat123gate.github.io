f9cb628634f5d7a642c0f8df71ab5bd1ee78f885
saikatmajumder444 <saikatmajumder444@gmail.com>
2021-09-07 18:11:54 +0530

    done

--- src/app.js
+++ src/app.js
@@ -296,6 +296,14 @@ app.post("/editnumber/verifyotp/:token", async (req, res) => {
 
 app.use("/", mainroute);
 
+
+
+app.get("/demo",(req,res)=>{
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+})
+
 app.get("*", (req, res) => {
   res.render("error", {
     isadmin: req.session.isAdmin,
